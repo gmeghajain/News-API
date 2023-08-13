c271ee0fa7bbdf6521bf092085937ae684db153c
megha gowadiya <megha@meghas-MacBook-Air.local>
2023-08-14 00:04:02 +0530

    add api to search and fetch

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ This is a simple API that interacts with the GNews API to fetch news articles ba
 1. Clone this repository.
 2. Install the required packages using `pip install -r requirements.txt`.
 3. Replace `'YOUR_GNEWS_API_KEY'` in `app.py` with your actual GNews API key.
+4. Install Redis server (Visit the Redis official website (https://redis.io/download) and download the latest stable release of Redis)
+5. Open a terminal and run cmd 'redis-server'
 
 ## Running the Application
 
--- app.py
+++ app.py
@@ -1,30 +1,55 @@
 from flask import Flask, request, jsonify
 import requests
 from cache import RedisCache
+import json
 
 app = Flask(__name__)
 cache = RedisCache()
 
 GNEWS_API_URL = "https://gnews.io/api/v4/search"
-# https://gnews.io/api/v4/{endpoint}?apikey=API_KEY "https://gnews.io/api/v4/search"
+GNEWS_TOP_HEADLINES_URL = "https://gnews.io/api/v4/top-headlines"
+API_KEY = 'ca34c717caaa6e1c1401ef9582dbaced' # Replace with your GNews API key
 
 @app.route('/news', methods=['GET'])
-def get_news():
-    keywords = request.args.get('keywords')
-    # cached_response = cache.get(keywords)
+def get_n_news_article():
+    n = request.args.get('max')
+    cached_response = cache.get(n)
     
-    # if cached_response:
-    #     return jsonify(cached_response)
+    if cached_response:
+        return jsonify(json.loads(cached_response))
     
     params = {
-        'q': keywords,
-        'token': 'ca34c717caaa6e1c1401ef9582dbaced',  # Replace with your GNews API key
+        'category': 'general',
+        'max': n,
+        'token': API_KEY
     }
+    response = requests.get(GNEWS_TOP_HEADLINES_URL, params=params)
+    articles = response.json().get('articles', [])
+    
+    cache.set(n, json.dumps(articles), timeout=3600)  # Cache for 1 hour
+    
+    return jsonify(articles)
+
+@app.route('/search', methods=['GET'])
+def get_news():
+    search_keyword = request.args.get('q')
+    search_in = request.args.get('in')
+
+    params = {
+        'q': search_keyword,
+        'in': search_in,
+        'token': API_KEY,
+    }
+    cache_key = search_keyword + search_in
+    cached_response = cache.get(cache_key)
+    
+    if cached_response:
+        return jsonify(json.loads(cached_response))
     
     response = requests.get(GNEWS_API_URL, params=params)
     articles = response.json().get('articles', [])
     
-    # cache.set(keywords, articles, timeout=3600)  # Cache for 1 hour
+    cache.set(cache_key, json.dumps(articles), timeout=3600)  # Cache for 1 hour
     
     return jsonify(articles)
 
